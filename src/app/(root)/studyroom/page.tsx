a09afa04a7cfeb3f7269fd2c0a840099b30c75be
seunghui-park <ridia1233@naver.com>
2025-02-16 20:22:23 +0900

    [#5] fix: 어드민 API 인증 적용
    - middleware.ts를 추가해 로그인되지 않은 사용자 접근 제한
    - fetchExtended.ts에 어드민 API 호출을 위한 커스텀 fetch 함수(adminFetchExtended) 추가

---
+++ src/app/(root)/studyroom/page.tsx
@@ -0,0 +1,7 @@
+export default async function StudyroomDashBoard() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4 md:p-8">
+      <h1 className="mb-6 text-2xl font-bold text-gray-800">스터디룸 관리</h1>
+    </div>
+  );
+}
