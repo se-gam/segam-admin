1b7da3eda80bcc9b709227ae17935b878cb4eb16
InGwonKang <tnqkr3494@naver.com>
2025-02-23 16:44:14 +0900

    [#3] fix: revalidate 재적용

--- src/lib/actions/crawler.ts
+++ src/lib/actions/crawler.ts
@@ -1,12 +1,16 @@
 'use server';
 
 import { adminFetchExtended } from '@/utils/fetchExtended';
-import { unstable_noStore } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 import { Crawler } from '../definitions';
 
 export async function getCrawler() {
-  unstable_noStore();
-  const { body: result } = await adminFetchExtended<Crawler>('/v1/batch/studyroom');
+  const { body: result } = await adminFetchExtended<Crawler>('/v1/batch/studyroom', {
+    cache: 'no-store',
+    next: {
+      tags: ['crawler'],
+    },
+  });
   return result;
 }
 
@@ -14,24 +18,28 @@ export async function activateCrawler() {
   await adminFetchExtended('/v1/batch/studyroom/activate', {
     method: 'POST',
   });
+  revalidateTag('crawler');
 }
 
 export async function deactivateCrawler() {
   await adminFetchExtended('/v1/batch/studyroom/deactivate', {
     method: 'POST',
   });
+  revalidateTag('crawler');
 }
 
 export async function activateCrawlerHealthCheck() {
   await adminFetchExtended('/v1/batch/studyroom/health-check/activate', {
     method: 'POST',
   });
+  revalidateTag('crawler');
 }
 
 export async function deactivateCrawlerHealthCheck() {
   await adminFetchExtended('/v1/batch/studyroom/health-check/deactivate', {
     method: 'POST',
   });
+  revalidateTag('crawler');
 }
 
 export async function changeCronTime(cronTime: string) {
@@ -39,4 +47,5 @@ export async function changeCronTime(cronTime: string) {
     method: 'POST',
     body: { cronTime },
   });
+  revalidateTag('crawler');
 }
