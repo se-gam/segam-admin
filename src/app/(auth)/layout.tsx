2462945e5482e9dd7cfd9254dc7da74121da273f
seunghui-park <ridia1233@naver.com>
2025-02-25 15:40:55 +0900

    [#10] refactor: 어드민 상단바 컴포넌트 분리

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '@/app/layout';
-
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <Layout>
-      <main>{children}</main>
-    </Layout>
-  );
-}
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,27 +1,10 @@
-import Link from 'next/link';
-import Layout from '@/app/layout';
+import Tabbar from '@/components/Tabbar';
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
-    <Layout>
-      <nav className="flex items-center justify-between px-8 py-2 shadow-md border-b border-gray-300 safe-area-top">
-        <h2 className="f16 font-bold text-theme_primary">Segam Admin</h2>
-          <div className="flex space-x-6">
-            <Link 
-              href="/notice"
-              className="f14 text-text_primary font-medium hover:text-theme_secondary transition-colors"
-            >
-              📢 공지사항
-            </Link>
-            <Link 
-              href="/studyroom"
-              className="f14 text-text_primary font-medium hover:text-theme_secondary transition-colors"
-            >
-              📚 스터디룸
-            </Link>
-          </div>
-      </nav>
+    <>
+      <Tabbar/>
       <main>{children}</main>
-    </Layout>
+    </>
   );
 }
